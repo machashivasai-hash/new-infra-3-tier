642569b74f9ab6507c8b4baa8d5a40c54382cae6
machashivasai-hash <machashivasai@gmail.com>
2025-12-05 11:57:09 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -21,7 +21,8 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0d176f79571d18a8f"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
+  key_name      = "devopsbyraham"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
